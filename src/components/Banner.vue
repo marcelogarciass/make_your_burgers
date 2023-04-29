efcbdce335fa5a594b62aae895fb8f6b186ebd71
marcelogarciass <marcelogarciass@hotmail.com>
2023-04-28 21:22:59 -0300

    verion 0.04

---
+++ src/components/Banner.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div id="main-banner">
+        <h1>Make Your Buger</h1>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Banner"
+}
+</script>
+
+<style scoped>
+    #main-banner {
+        background-image: url('/public/img/burger.jpg');
+        background-position: 0 -250px;
+        background-size: cover;
+        height: 500px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <template>
+  <Banner/>
   <div class="main-container">
-   <h1>Home</h1>
+   <h1>Monte o seu burger</h1>
+   <P>Formulário</P>
   </div>
 </template>
 
 <script>
+import Banner from '@/components/Banner.vue';
+
 export default {
-  
+    components: { Banner }
 }
 </script>
