ca3df422d25f48976e163fb485fb04514ec25d0c
marcelogarciass <marcelogarciass@hotmail.com>
2023-04-29 22:33:15 -0300

    version 0.06

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,15 @@
   <Banner/>
   <div class="main-container">
    <h1>Monte o seu burger</h1>
-   <P>Formulário</P>
+   <burger-form/>
   </div>
 </template>
 
 <script>
 import Banner from '@/components/Banner.vue';
+import BurgerForm from '@/components/BurgerForm.vue';
 
 export default {
-    components: { Banner }
+    components: { Banner, BurgerForm }
 }
 </script>
