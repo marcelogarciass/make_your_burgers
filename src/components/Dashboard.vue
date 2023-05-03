2a1e6068a02ba4f720a826ad2205a27925c0f558
marcelogarciass <marcelogarciass@hotmail.com>
2023-05-03 11:52:00 -0300

    Recebendo o Status

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -25,6 +25,7 @@
                 <div>
                     <select name="status" class="status">
                         <option value="">Selecione</option>
+                        <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo" > {{ s.tipo }} </option>
                     </select>
                     <button class="delete-btn" >Cancelar</button>
                 </div>
@@ -54,6 +55,15 @@ export default {
             console.log(this.burgers);
 
             // resgatar o status
+            this.getStatus();
+        },
+        async getStatus() {
+
+            const req = await fetch("http://localhost:3000/status");
+
+            const data = await req.json();
+
+            this.status = data;
         }
     },
     mounted() {
